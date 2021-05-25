88b9813448928fb58b355be2c2a3ca81b1a9e375
choibyunghyeon <choi8608@gmail.com>
2021-05-26 06:15:58 +0900

    Initial commit

---
+++ src/test/java/com/example/urlshortening/UrlShorteningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.urlshortening;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlShorteningApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
