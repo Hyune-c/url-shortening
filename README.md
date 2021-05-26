63bdf2d03baee6b2ee23f24aed322036e3b2635c
choibyunghyeon <choi8608@gmail.com>
2021-05-26 11:10:22 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# URL Shortening 을 만들어보자!
+
+## 환경
+
+- springboot 2.5.0
--- src/main/java/com/example/urlshortening/UrlShorteningApplication.java
+++ src/main/java/com/example/urlshortening/UrlShorteningApplication.java
@@ -2,6 +2,7 @@ package com.example.urlshortening;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 public class UrlShorteningApplication {
@@ -9,5 +10,4 @@ public class UrlShorteningApplication {
   public static void main(String[] args) {
     SpringApplication.run(UrlShorteningApplication.class, args);
   }
-
 }
