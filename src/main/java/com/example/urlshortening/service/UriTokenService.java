47064808b94f4a49195abb7cec1e0e9e92dbc5e2
choibyunghyeon <choi8608@gmail.com>
2021-05-29 21:45:22 +0900

    #8 ♻️ cache 적용 전 변경/조회 로직 분리

--- src/main/java/com/example/urlshortening/service/UriTokenService.java
+++ src/main/java/com/example/urlshortening/service/UriTokenService.java
@@ -1,6 +1,10 @@
 package com.example.urlshortening.service;
 
+import static com.example.urlshortening.common.exception.ErrorCode.NOT_FOUND;
+
+import com.example.urlshortening.common.exception.custom.BusinessException;
 import com.example.urlshortening.entity.UriTokenEntity;
+import com.example.urlshortening.repository.UriTokenRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -11,17 +15,19 @@ import org.springframework.transaction.annotation.Transactional;
 public class UriTokenService {
 
   private final CreateUriTokenService createUriTokenService;
-  private final FindUriTokenService findUriTokenService;
+  private final UriTokenRepository uriTokenRepository;
 
   public String encode(String uri) {
     Long uriTokenId = createUriTokenService.create(uri);
-    UriTokenEntity uriTokenEntity = findUriTokenService.findById(uriTokenId);
+    UriTokenEntity uriTokenEntity = uriTokenRepository.findById(uriTokenId)
+        .orElseThrow(() -> new BusinessException(NOT_FOUND));
 
     return uriTokenEntity.getUriToken();
   }
 
-  public String decode(String token) {
-    UriTokenEntity uriTokenEntity = findUriTokenService.findByUriToken(token);
+  public String decode(String uriToken) {
+    UriTokenEntity uriTokenEntity = uriTokenRepository.findByUriToken(uriToken)
+        .orElseThrow(() -> new BusinessException(NOT_FOUND));
     uriTokenEntity.addCount();
 
     return uriTokenEntity.getUri();
